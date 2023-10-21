e396f9d91dde9d0017cd6b543f5cbfeaf787b9e1
Geroprez <128936541+Geroprez@users.noreply.github.com>
2023-10-21 19:33:25 -0300

    Add files via upload

---
+++ Personaje.js
@@ -0,0 +1,50 @@
+class personaje {
+  constructor() {
+  this.DERECHA = "DERECHA";
+  this.IZQUIERDA = "IZQUIERDA";
+  this.ARRIBA = "ARRIBA";
+  this.ABAJO = "ABAJO";
+  
+  this.posX = 60;
+  this.posY = 45;
+  this.dir = this.DERECHA;
+  this.tam = 10;
+  this.vel = 1;
+  this.imagenDer = ImagenesDer;
+  this.imagenIzq = ImagenesIzq;
+  this.imagenUp = ImagenesUp;
+  this.imagenDown = ImagenesDown;
+  this.spriteActual = 0;
+  
+  
+  
+  }
+  
+
+
+
+ moverIzquierda () {
+   
+ }
+
+ moverDerecha () {
+
+ }
+ 
+ moverArriba() {
+ }
+ 
+ moverAbajo() {
+ }
+ 
+ 
+ ponerBomba() {
+   
+ }
+ 
+ Bomba() {
+   
+ }
+
+
+}
